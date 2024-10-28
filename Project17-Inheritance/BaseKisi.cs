e79fd8cad4c6aa7b0b65b98dcf4a2882af2f5a0d
Eda Özge UĞURLU <edaozgeugurlu@gmail.com>
2024-10-28 12:40:22 +0300

    Proje dosyası ekle.

---
+++ Project17-Inheritance/BaseKisi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project17_Inheritance
+{
+    // Temel sınıf: Bir kişiyi temsil eder
+    public class BaseKisi
+    {
+        // Ad ve soyad için özellikler
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        // Ad ve soyadı ekrana yazdıran sanal metot
+        public virtual void BilgiYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}");
+        }
+    }
+
+    // Öğrenci için türetilmiş sınıf
+    public class Ogrenci : BaseKisi
+    {
+        // Öğrenci numarası için özellik
+        public int OgrenciNo { get; set; }
+
+        // Öğrenci bilgilerini ekrana yazdıran metot
+        public void OgrenciBilgileriYazdir()
+        {
+            Console.WriteLine($"Öğrenci No: {OgrenciNo}, {Ad} {Soyad}");
+        }
+    }
+
+    // Öğretmen için türetilmiş sınıf
+    public class Ogretmen : BaseKisi
+    {
+        // Maaş bilgisi için özellik
+        public decimal Maas { get; set; }
+
+        // Öğretmen bilgilerini ekrana yazdıran metot
+        public void OgretmenBilgileriYazdir()
+        {
+            Console.WriteLine($"Maaş: {Maas} TL, {Ad} {Soyad}");
+        }
+    }
+}
+
---
+++ Project17-Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿// Ana program
+using Project17_Inheritance;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Ogrenci (Öğrenci) nesnesi oluşturma
+        Ogrenci ogrenci = new Ogrenci
+        {
+            Ad = "Yıldız",             // Öğrencinin adını ayarlama
+            Soyad = "Tilbe",           // Öğrencinin soyadını ayarlama
+            OgrenciNo = 12345         // Öğrenci numarasını ayarlama
+        };
+
+        // Ogretmen (Öğretmen) nesnesi oluşturma
+        Ogretmen ogretmen = new Ogretmen
+        {
+            Ad = "Sezen",           // Öğretmenin adını ayarlama
+            Soyad = "Aksu",         // Öğretmenin soyadını ayarlama
+            Maas = 17002            // Öğretmenin maaşını ayarlama
+        };
+
+        // Öğrenci bilgilerini yazdırma
+        ogrenci.OgrenciBilgileriYazdir();
+
+        // Öğretmen bilgilerini yazdırma
+        ogretmen.OgretmenBilgileriYazdir();
+    }
+}
